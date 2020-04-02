f5a3cb64777360483d01db751155d67cf8f79332
Marcos Alvarez <marcosalvarez@iMac-de-Marcos.local>
2020-04-02 19:05:28 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="blackCard">
+      <Card color="black" :text="this.blackCard" />
+    </div>
+    <div class="whiteCards">
+      <Card
+        :key="key"
+        v-for="(card, key) in this.whiteCards"
+        :text="card"
+        color="white"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
+import cards from "./assets/cards.json";
+
+let pickBlackCard = () => {
+  let random = Math.random() * (10 - 0) + 0;
+  return cards.blackCards[Math.round(random)].text;
+};
+
+let pickWhiteCards = () => {
+  let wCards = [];
+  for (let i = 0; i < 10; i++)
+    wCards.push(
+      cards.whiteCards[Math.floor(Math.random() * cards.whiteCards.length)]
+    );
+  return wCards;
+};
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Card
+  },
+  data() {
+    return {
+      allWhiteCards: cards.whiteCards,
+      allBlackCards: cards.blackCards,
+      blackCard: pickBlackCard(),
+      whiteCards: pickWhiteCards()
+    };
   }
-}
+};
 </script>
 
 <style>
+body {
+  background-color: #e0e0e0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +59,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.blackCard {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.whiteCards {
+  display: flex;
+  flex-flow: row wrap;
+  padding: 0 2em;
+  margin: 2em 0;
+  justify-content: center;
+  align-items: center;
+}
+
+.whiteCards .card {
+  margin: 1em;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card" :class="this.color">
+    <div class="body">
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+    color: String
+  }
+};
+</script>
+<style scoped>
+.card {
+  height: 350px;
+  width: 200px;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 0 1em;
+  font-size: 1.5em;
+  font-weight: bolder;
+  text-align: left;
+}
+
+.black {
+  background-color: black;
+  color: white;
+}
+
+.white {
+  background-color: white;
+  color: black;
+  cursor: pointer;
+  transition: all 200ms ease-in-out;
+}
+
+.white:hover {
+  transform: scale(1.1);
+}
+</style>
